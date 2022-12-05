c46a7590b63ffe678fa5606326f8b261d1f11bb2
mechauk418 <mechauk418@gmail.com>
2022-12-05 11:55:15 +0900

    fix: 회원가입 로직을 수정한다

--- unb/src/main.js
+++ unb/src/main.js
@@ -3,5 +3,7 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import axios from '../src/axios'
 
 createApp(App).use(store).use(router).mount('#app')
+axios.defaults.headers.common.Authorization = 'Bearer ' + localStorage.getItem('access_token')
