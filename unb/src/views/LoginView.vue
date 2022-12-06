d22ef3d3ac4efb4e2f4d1852eb4551a96ffbaeb6
mechauk418 <mechauk418@gmail.com>
2022-12-06 23:22:14 +0900

    feat: 유저 디테일을 추가한다

--- unb/src/views/LoginView.vue
+++ unb/src/views/LoginView.vue
@@ -1,23 +1,24 @@
 <template>
-  <div class="login-container my-shadow">
-    <form class="myform">
-      <h1 class="form-title">로그인</h1>
-      <div class="input-wrap">
-        <label for="email">이메일</label>
-        <input type="text" id="email" v-model="email" class="input-text"/>
-      </div>
-      <div class="input-wrap">
-        <label for="password">패스워드</label>
-        <input type="password" id="password" v-model="password" class="input-text"/>
-      </div>
-    <button @click="loginSubmit()" class="form-btn my-shadow">로그인</button>
-    <button @click="logoutplz()" class="form-btn my-shadow">로그아웃</button>
+  <div class="app">
+    <div>
+      <label for="email">
+        <span>이메일</span>
+        <input type="text" id="email" v-model="email" />
+      </label>
+      <label for="password">
+        <span>패스워드</span>
+        <input type="password" id="password" v-model="password" />
+      </label>
+      <hr />
+      <button @click="loginSubmit()">로그인</button>
+    </div>
+    <button @click="logoutplz()">로그아웃</button>
     <button @click="googlelogin()">구글로그인</button>
     <button @click="kakaologin()">카카오</button>
-    <a href="https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/userinfo.email&response_type=code&client_id={client_id}&redirect_uri={redirect_uri}">
+    <a href="https://accounts.google.com/o/oauth2/v2/auth?client_id=594914704717-2s7b5k1fjai3o89vnc66hc0fiisa15uq.apps.googleusercontent.com&response_type=code&redirect_uri=http://localhost:8000/accounts/google/callback/&scope=https://www.googleapis.com/auth/userinfo.email"
+>
     구글로그인
     </a>
-    </form>
   </div>
 </template>
 
@@ -41,12 +42,7 @@ export default {
       this.$store.dispatch('logouttest_act')
     },
     googlelogin() {
-      axios.get('http://localhost:8000/accounts/google/login', { headers: {
-        'Access-Control-Allow-Origin': '*',
-        Authorization: null
-        }
-      }
-      )
+      axios.get('http://localhost:8000/accounts/google/login')
       .then((response) => {
         console.log(response.data)
       }
@@ -58,13 +54,8 @@ export default {
     },
     kakaologin() {
       window.location.href = 'http://localhost:8000/accounts/kakao/login'
+
     }
   }
 }
 </script>
-<style scoped>
-.login-container {
-  width: 550px;
-  margin: 200px auto;
-}
-</style>
