2b10b4a4ae0ffba132c63119cb6d6124db549733
mechauk418 <mechauk418@gmail.com>
2022-12-07 23:35:12 +0900

    feat: 상세보기를 작성한다

--- unb/src/views/AllListView.vue
+++ unb/src/views/AllListView.vue
@@ -1,20 +1,35 @@
 <template>
   <div>
     <TodayKing></TodayKing>
-    <div v-for="article in articles" :key="article.id">
-      <router-link
-      :to="{name: 'balance-game', params: {slug: article.slug}}"
-      >
-        {{ article.title }}
-        {{ article.content }}
-      </router-link>
+    <h1> 토론리스트 </h1>
+    <div>
+      <div class="listWrapper">
+        <div v-for="(article, index) in articles" :key="index" class="list">
+          <router-link :to="'/Detail/' + article.pk">
+            <div class="box">
+              <div class="table-row">
+                <div class="divA">{{ article.A }}</div>
+                <div class="divVS">VS</div>
+                <div class="divB">{{article.B}}</div>
+              </div>
+              <div>{{article.ABcount.A_percent}} {{article.ABcount.B_percent}} (A B 퍼센트) </div>
+              <div class="test">
+                <div class="A_bar" :style="`width:${article.ABcount.A_percent}%`" style="background-color:rgb(199, 92, 167)">AAAAA</div>
+                <div class="B_bar" :style="`width:${article.ABcount.B_percent}%`" style="background-color:rgb(18, 151, 213);">BBBBB</div>
+              </div>
+            </div>
+          </router-link>
+        </div>
+      </div>
+      <!-- <router-view :el="el"></router-view> -->
     </div>
   </div>
 </template>
 <script>
 import {csrftoken} from '../csrf/csrf_token'
 import TodayKing from '../components/TodayKing'
-import url from '../main'
+import testaxios from '../axios/index'
+const url = 'http://localhost:8000/articles/'
 export default {
   components: {
     TodayKing
@@ -22,33 +37,59 @@ export default {
   data () {
     return {
       articles: [],
+      comments:[]
     }
   },
   setup () {},
   created () {},
-  mounted () {},
-  unmounted () {},
-  methods: {
-    getAllArticles() {
-      fetch('http://localhost:8000/articles/', {
-        methods: "GET",
-        headers: {
-          'Content-Type': 'application/json',
-          'X-CSRFTOKEN': csrftoken
-        }
+  mounted() {
+    testaxios ({
+      method: 'GET',
+      url: url,
+      headers: {
+        Authorization: 'Bearer ' + localStorage.getItem('access_token')
+      }
+    })
+      .then(response => {
+        this.articles = response.data
+        console.log(response.data)
       })
-      .then(resp => resp.json())
-      .then((data) => {
-        // 위에 data에 있는 articles 리스트에 게시글들을 담음
-        // articles 리스트에 있는 데이터들을 template의 v-for로 반복문을 돌려서 뽑아냄
-        this.articles.push(...data)
-        console.log(data)
+      .catch(response => {
+        console.log('access토큰 만료')
+        console.log(response.response.data)
       })
-      .catch(error => console.log(error))
-    },
-    create() {
-      this.getAllArticles()
-    }
-  }
+    testaxios ({
+      method: 'GET',
+      url: url,
+      headers: {
+        Authorization: 'Bearer ' + localStorage.getItem('access_token')
+      }
+    })
+      .then(response => {
+        this.articles = response.data
+        console.log(response.data)
+      })
+      .catch(response => {
+        console.log('access토큰 만료')
+        console.log(response.response.data)
+      })
+  },
+  unmounted() {},
+  methods: {}
 }
+
 </script>
+<style scoped>
+.table-row {
+  height: 50%;
+  display: grid;
+  grid-template-columns: 4fr 1fr 4fr;
+}
+.box {
+  border: 1px solid black;
+  height: 150px;
+}
+.test{
+  display: flex;
+}
+</style>
