2072f2b7cd17df2befa925d4562709eb0a412e6b
dortkthf <dortkthf@gmail.com>
2022-12-12 23:20:45 +0900

    feat : about

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,10 @@
   <div class="home">
     <TodayKing></TodayKing>
     <!-- 밸런스 게임 바로가기 -->
-    <a :href="'/Detail/'+random_index">
+    <router-link to="/about">
+      <p style="text-align: start;">❓언발란스에 대해 알아보기></p>
+    </router-link>
+    <a :href="'/Detail/' + random_index">
       <div class="balance-wrap">
         <div class="balance-back my-shadow" style="background-color: #FF719B;"></div>
         <div class="balance-back my-shadow" style="background-color: #4BBEFF; align-self: flex-end;"></div>
@@ -24,36 +27,36 @@
       <div class="today-dis-article-wrap">
         <img src="../assets/vs.png" class="vs">
         <article>
-          <a :href="'/Detail/'+today_pk">
-            <h1 class="article-title">{{article_A}}</h1>
+          <a :href="'/Detail/' + today_pk">
+            <h1 class="article-title">{{ article_A }}</h1>
           </a>
-          <a :href="'/Detail/'+today_pk">
+          <a :href="'/Detail/' + today_pk">
             <div class="today-dis-comment-wrap">
               <img src="../assets/말풍선1.png">
               <div class="comments">
                 <div class="best-comment">
                   <div style="background-color:#FF719B;">BEST</div>
-                  <p class="best-p">{{this.best_A}}</p>
+                  <p class="best-p">{{ this.best_A }}</p>
                 </div>
-                <p v-for="(item,index) in comments_A.slice(0,3)" :key="index">{{item}}</p>
+                <p v-for="(item, index) in comments_A.slice(0, 3)" :key="index">{{ item }}</p>
               </div>
             </div>
           </a>
         </article>
 
         <article>
-          <a :href="'/Detail/'+today_pk">
-            <h1 class="article-title">{{article_B}}</h1>
+          <a :href="'/Detail/' + today_pk">
+            <h1 class="article-title">{{ article_B }}</h1>
           </a>
-          <a :href="'/Detail/'+today_pk">
+          <a :href="'/Detail/' + today_pk">
             <div class="today-dis-comment-wrap">
               <img src="../assets/말풍선2.png">
               <div class="comments">
                 <div class="best-comment">
                   <div style="background-color:#4BBEFF;">BEST</div>
-                  <p class="best-p">{{this.best_B}}</p>
+                  <p class="best-p">{{ this.best_B }}</p>
                 </div>
-                <p v-for="(item,index) in comments_B.slice(0,3)" :key="index">{{item}}</p>
+                <p v-for="(item, index) in comments_B.slice(0, 3)" :key="index">{{ item }}</p>
               </div>
             </div>
           </a>
@@ -70,52 +73,52 @@ export default {
   components: {
     TodayKing
   },
-  data () {
+  data() {
     return {
       sampleData: '',
       random_index: '',
-      today_pk:'',
-      article_A:'',
-      article_B:'',
-      comment:'',
-      comments_A:[],
-      comments_B:[],
-      best_A:'',
-      best_B:''
+      today_pk: '',
+      article_A: '',
+      article_B: '',
+      comment: '',
+      comments_A: [],
+      comments_B: [],
+      best_A: '',
+      best_B: ''
     }
   },
-  setup () {},
-  created () {},
-  mounted () {
+  setup() { },
+  created() { },
+  mounted() {
     axios.get(url + 'random/article/')
-    .then((response) =>{
-      this.random_index = response.data.article_pk
-    })
+      .then((response) => {
+        this.random_index = response.data.article_pk
+      })
 
     axios.get('https://www.unbback.cf/articles/today/article/') // 오늘의 토론 URL 넣을 자리
-    .then((response) =>{
-      this.today_pk = response.data.article_pk
-      axios({ // 글 axios
-      method: 'GET',
-      url: `https://www.unbback.cf/articles/${this.today_pk}/`
+      .then((response) => {
+        this.today_pk = response.data.article_pk
+        axios({ // 글 axios
+          method: 'GET',
+          url: `https://www.unbback.cf/articles/${this.today_pk}/`
+        })
+          .then(response => {
+            this.article_A = response.data.A
+            this.article_B = response.data.B
+            this.comment = response.data.comments
+            for (const obj of this.comment) {
+              if (obj.pick == 1) {
+                this.comments_A.push(obj.content)
+              } else if (obj.pick == 2) {
+                this.comments_B.push(obj.content)
+              }
+            }
+            this.best_A = response.data.best_A.content
+            this.best_B = response.data.best_B.content
+          })
       })
-      .then(response => {
-        this.article_A = response.data.A
-        this.article_B = response.data.B
-        this.comment = response.data.comments
-        for (const obj of this.comment){
-          if (obj.pick == 1) {
-            this.comments_A.push(obj.content)
-          } else if ( obj.pick == 2) {
-            this.comments_B.push(obj.content)
-          }
-        }
-        this.best_A = response.data.best_A.content
-        this.best_B = response.data.best_B.content
-      })
-    })
   },
-  unmounted () {},
+  unmounted() { },
   methods: {
   }
 }
@@ -127,49 +130,60 @@ export default {
   height: 460px;
   margin-bottom: 50px;
 }
+
 .balance-back {
   width: 50%;
   height: 450px;
 }
+
 .balance-title {
   position: absolute;
   color: white;
   left: 50%;
   transform: translate(-50%, -50%);
 }
+
 .b-title {
   top: 39%;
   font-weight: 700;
   font-size: 50px;
 }
+
 .b-subtitle {
   top: 55%;
   font-weight: 300;
   font-size: 20px;
 }
+
 .today-dis {
   margin: 50px 0;
 }
+
 .today-dis-title {
   display: flex;
   justify-content: space-between;
   align-items: flex-end;
 }
+
 .today-dis-title>h1 {
   font-size: 28px;
   margin: 0;
 }
+
 .today-dis-title>a {
   text-decoration: none;
   color: gray;
 }
+
 .today-dis-title p {
   margin: 0;
 }
+
 .today-dis-article-wrap {
   display: flex;
   position: relative;
 }
+
 .vs {
   position: absolute;
   top: 65%;
@@ -177,17 +191,20 @@ export default {
   transform: translate(-50%, -50%);
   width: 60px;
 }
+
 .article-title {
   /* width: 90%;
   margin: 0 auto; */
   font-size: 20px;
 }
-.today-dis-comment-wrap{
+
+.today-dis-comment-wrap {
   display: flex;
   flex-direction: column;
   position: relative;
   height: 400px;
 }
+
 .today-dis-comment-wrap img {
   position: absolute;
   z-index: -1;
@@ -197,6 +214,7 @@ export default {
   height: 400px;
   width: 80%;
 }
+
 .today-dis-comment-wrap>div>p {
   margin: 10px 0;
   text-align: start;
@@ -204,11 +222,13 @@ export default {
   overflow: hidden;
   text-overflow: ellipsis;
   display: -webkit-box;
-  -webkit-line-clamp: 3; /* 라인수 */
+  -webkit-line-clamp: 3;
+  /* 라인수 */
   -webkit-box-orient: vertical;
-  word-wrap:break-word;
+  word-wrap: break-word;
   line-height: 1.2em;
 }
+
 .best-p {
   margin: 10px 0;
   text-align: start;
@@ -216,17 +236,20 @@ export default {
   overflow: hidden;
   text-overflow: ellipsis;
   display: -webkit-box;
-  -webkit-line-clamp: 3; /* 라인수 */
+  -webkit-line-clamp: 3;
+  /* 라인수 */
   -webkit-box-orient: vertical;
-  word-wrap:break-word;
+  word-wrap: break-word;
   line-height: 1.2em;
 }
+
 .best-comment {
   display: flex;
   align-items: center;
   justify-content: center;
   margin-bottom: 10px;
 }
+
 .best-comment div {
   color: white;
   border-radius: 5px;
@@ -235,9 +258,11 @@ export default {
   font-family: 'Pretendard-Regular';
   font-size: 14px;
 }
+
 article {
   width: 50%;
 }
+
 .comments {
   position: absolute;
   z-index: 1;
---
+++ src/views/about.vue
@@ -0,0 +1,92 @@
+<template>
+    <div>
+
+        <div class="about kg-font">
+            <h1 class="">활동 점수</h1>
+            <div style="text-align:start">
+                <p>토론 글 작성 : 20점을 받을 수 있어요</p>
+                <p>밸런스 게임 선택 : 10점을 받을 수 있어요</p>
+                <p>댓글 작성 : 5점을 받을 수 있어요</p>
+            </div>
+        </div>
+        <div style="margin:2rem 0rem 4rem 0rem">
+            <div style="display:flex">
+                <img src="../../public/badges/base.png" alt="" class="badge">
+                <p style="display:flex; align-items:center" class="badge_title">기본 뱃지 : 가입 즉시 받을 수 있어요.</p>
+            </div>
+            <div style="display:flex">
+                <img src="../../public/badges/bronze.png" alt="" class="badge">
+                <p style="display:flex; align-items:center" class="badge_title">브론즈 뱃지 : 브론즈 등급을 달성하면 드려요. 브론즈 등급은 활동점수
+                    30을 기록하면 달성 할 수 있어요
+                </p>
+            </div>
+            <div style="display:flex">
+                <img src="../../public/badges/silver.png" alt="" class="badge">
+                <p style="display:flex; align-items:center" class="badge_title">실버 뱃지 : 실버 등급을 달성하면 드려요. 실버 등급은 활동점수
+                    300을 기록하면 달성 할 수 있어요
+                </p>
+            </div>
+            <div style="display:flex">
+                <img src="../../public/badges/gold.png" alt="" class="badge">
+                <p style="display:flex; align-items:center" class="badge_title">골드 뱃지 : 골드 등급을 달성하면 드려요. 골드 등급은 활동점수
+                    600을 기록하면 달성 할 수 있어요
+                </p>
+            </div>
+            <div style="display:flex">
+                <img src="../../public/badges/platinum.png" alt="" class="badge">
+                <p style="display:flex; align-items:center" class="badge_title">플레티넘 뱃지 : 플레티넘 등급을 달성하면 드려요. 플레티넘 등급은
+                    활동점수 1000을 기록하면 달성 할 수
+                    있어요</p>
+            </div>
+            <div style="display:flex">
+                <img src="../../public/badges/dia.png" alt="" class="badge">
+                <p style="display:flex; align-items:center" class="badge_title">다이아 뱃지 : 다이아 등급을 달성하면 드려요. 다이아 등급은 활동점수
+                    1600을 기록하면 달성 할 수
+                    있어요</p>
+            </div>
+            <div style="display:flex">
+                <img src="../../public/badges/master.png" alt="" class="badge">
+                <p style="display:flex; align-items:center" class="badge_title">마스터 뱃지 : 최고 등급인 마스터 등급을 달성하면 드려요. 마스터
+                    등급은 활동점수 2500을 기록하면 달성
+                    할 수 있어요</p>
+            </div>
+            <div style="display:flex">
+                <img src="" alt="" class="badge">
+                <p style="display:flex; align-items:center" class="badge_title">토론왕 뱃지 : 그 날의 토론왕에게 드려요. 하루 중 가장 높은
+                    활동점수를 기록하면 토론왕이 될 수 있어요</p>
+            </div>
+            <div style="display:flex">
+                <img src="" alt="" class="badge">
+                <p style="display:flex; align-items:center" class="badge_title">KDT 뱃지 : 운영진에게 요청하면 드려요. 운영진과 친분이 있으신분들은
+                    받을 수 있을거에요</p>
+            </div>
+        </div>
+    </div>
+</template>
+<style scoped>
+.about {
+    max-width: 50rem;
+    margin: 120px 0px 30px 0px;
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    border-bottom: 5px solid #4BBEFF;
+    border-right: 5px solid #4BBEFF;
+    border-top: 5px solid #FF719B;
+    border-left: 5px solid #FF719B;
+    padding: 3vmin 10vmin;
+    font-weight: bold;
+}
+
+.badge {
+    width: 7rem;
+    height: 7rem;
+    margin-right: 2rem
+}
+
+.badge_title {
+    font-size: 15px;
+    font-weight: bold;
+}
+</style>
